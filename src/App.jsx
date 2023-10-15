dca990e3ca0fc0153f983e40be37a6b387dbccef
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-15 01:54:10 -0700

    add the two major component inside app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import NavigationMenu from "./component/NavigationMenu"
+import MainBody from "./component/MainBody"
+import classes from "./App.module.css"
 export default function  App () {
 
   return (
-    <NavigationMenu/>
+    <div className={classes.whole}>
+      <NavigationMenu/>
+      <MainBody/>
+    </div>
   )
 }
