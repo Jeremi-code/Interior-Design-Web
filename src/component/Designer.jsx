fff04cd8a5a5bb9f5ed1ad2ac82c7d5ee8bca9f9
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 11:39:56 -0700

    add font-weight for Jsx element

--- src/component/Designer.jsx
+++ src/component/Designer.jsx
@@ -6,6 +6,7 @@ const Designer = () => {
     <hr className={horizontal.hr}/>
     <p>The best team in the world.<br/><br/>
     We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p style={{fontWeight:"bolder"}}>our designers are thoughtfully choosen</p>
   </div>
     )
 }
