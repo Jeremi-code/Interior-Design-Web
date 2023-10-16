57c6cf027ceef2ef49a8a4b2beed85d331be3a78
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 13:32:49 -0700

    call package component inside this component

--- src/component/MainBody.jsx
+++ src/component/MainBody.jsx
@@ -2,12 +2,14 @@ import classes from '../css/MainBody.module.css'
 import ShowCase from './ShowCase'
 import Services from './Services'
 import Designer from './Designer'
+import Packages from './Packages'
 export default function MainBody () {
     return (
         <div className={classes.main}>
             <ShowCase/>
             <Services/>
             <Designer/>
+            <Packages/>
         </div>
     )
 }
