fdcdb51ca4741ed3cfc5dff03c273258bd77ac1f
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 11:25:05 -0700

    add Designer component inside this component

--- src/component/MainBody.jsx
+++ src/component/MainBody.jsx
@@ -1,11 +1,13 @@
 import classes from '../css/MainBody.module.css'
 import ShowCase from './ShowCase'
 import Services from './Services'
+import Designer from './Designer'
 export default function MainBody () {
     return (
         <div className={classes.main}>
             <ShowCase/>
             <Services/>
+            <Designer/>
         </div>
     )
 }
