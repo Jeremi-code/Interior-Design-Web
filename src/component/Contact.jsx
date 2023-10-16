b848bce60a33a21b3d1b2fcfa865639f0c5d11c6
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 14:59:46 -0700

    create contact component

---
+++ src/component/Contact.jsx
@@ -0,0 +1,11 @@
+import horizontal from '../css/MainBody.module.css'
+const Contact = () => {
+  return (
+    <div>
+        <h3>Contacts</h3>
+        <hr className={horizontal.hr}/>
+    </div>
+  )
+}
+
+export default Contact
