b0c678018bef19a661d1f1f1975a39b6ea2418ec
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 13:33:06 -0700

    add some content to this component

--- src/component/Packages.jsx
+++ src/component/Packages.jsx
@@ -1,7 +1,11 @@
-
+import horizontal from '../css/MainBody.module.css'
 const Packages = () => {
   return (
-
+    <div>
+        <h3>Packages</h3>
+        <hr className={horizontal.hr}/>
+        
+    </div>
   )
 }
 
