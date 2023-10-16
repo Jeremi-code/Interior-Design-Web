738a1ee0bf1ffe4c8229717f5594dd47c70da770
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 03:43:32 -0700

    call Service component inside this component

--- src/component/MainBody.jsx
+++ src/component/MainBody.jsx
@@ -1,9 +1,11 @@
 import classes from '../css/MainBody.module.css'
 import ShowCase from './ShowCase'
+import Services from './Services'
 export default function MainBody () {
     return (
         <div className={classes.main}>
             <ShowCase/>
+            <Services/>
         </div>
     )
 }
