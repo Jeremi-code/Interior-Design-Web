1afbfd057880cd256fd75c06ec9624ac52e5b34b
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-16 11:19:18 -0700

    add contents to Designer component

--- src/component/Designer.jsx
+++ src/component/Designer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Designer = () => {
   return (
-  )
+  <div>
+    <h3>Designers</h3>
+    <p>The best team in the world.<br/><br/>
+    We are lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  </div>
+    )
 }
 
 export default Designer
