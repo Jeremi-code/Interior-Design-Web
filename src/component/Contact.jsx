d97816e9992b8268c5a4c11b24a8c8caed09c9fc
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-17 00:24:43 -0700

    add form inside this component for recieving user input

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,9 +1,20 @@
 import horizontal from '../css/MainBody.module.css'
+import classes from '../css/MainBody.module.css'
 const Contact = () => {
   return (
-    <div>
+    <div >
         <h3>Contacts</h3>
         <hr className={horizontal.hr}/>
+        <p>Do you want us to style your home? Fill out the form and fill me in with the details :) We love meeting new people!</p>
+        <form className={classes.contact}>
+            <label for="name">name</label>
+            <input type="text"/>
+            <label for="email">email</label>  
+            <input type="text"/>
+            <label for="Message">Message</label >
+            <input type="text"/>
+            <input type="submit" value="Send Message"/>
+        </form>
     </div>
   )
 }
