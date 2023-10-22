89443afad14ee0f79ad210a1a184145dfda90333
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-22 03:30:47 -0700

    create express server, add middleware and add route

---
+++ back-end/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { sendMessage} from './controller/MessageController.js'
+import connectDB from './config/db.js'
+const port = process.env.port || 5000
+dotenv.config()
+connectDB()
+const app = express()
+console.log (sendMessage)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.post("/sendMessage",sendMessage)
+
+app.listen(port , () => {
+    console.log(`server started on ${port}`)
+})
+// import express from 'express'
+// const app = express()
+// app.get('/about',(req,res) => {
+    // res.send("what is happening over here")
+    // res.set({'Content-Type' : 'application/json'})
+    // console.log(res.header['Content-Type'])
+    // console.log(req.headers[)
+    // res.json(["warmonger"])
+    // res.render('about',{name : 'flavio'})
+// })
+// app.listen(5000,()=> {
+//     console.log("server started on port 5000")
+// })
