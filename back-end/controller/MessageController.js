f4060f1b4ec446d0ac71f9e1d956dc5bb1145b47
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 10:02:36 -0700

    format the code

--- back-end/controller/MessageController.js
+++ back-end/controller/MessageController.js
@@ -1,21 +1,23 @@
-import User from "../models/model.js"
-import asyncHandler from 'express-async-handler'
+import User from "../models/model.js";
+import asyncHandler from "express-async-handler";
 
-const sendMessage = asyncHandler( async (req,res) => {
-    const { username,email,message } = req.body
-    const user = await User.create({username,email,message})
-    console.log(user)
-    if (user) {
-        res.status(201).json({
-            _id : user._id,
-            name: user.name,
-            email : user.email
+const sendMessage = asyncHandler(async (req, res) => {
+  console.log(typeof(req))
+  
+  const { name, email, message } = JSON.parse(req.body);
+  console.log(req.body);
+  const user = await User.create({ name, email, message });
+  console.log(user);
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid error");
+  }
+});
 
-        })
-    } else {
-        res.status(400)
-        throw new Error ("Invalid error")
-    }   
-})
-
-export { sendMessage}
+export { sendMessage };
