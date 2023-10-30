89f27412459087ee4a89bbfad6a5f3d0fb705063
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 10:19:22 -0700

    remove unwanted comments

--- back-end/server.js
+++ back-end/server.js
@@ -2,31 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import { sendMessage } from "./controller/MessageController.js";
 import connectDB from "./config/db.js";
-import cors from "cors"
+import cors from "cors";
 const port = process.env.port || 5000;
 dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors())
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.post("/sendMessage", sendMessage);
 
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 });
-// import express from 'express'
-// const app = express()
-// app.get('/about',(req,res) => {
-// res.send("what is happening over here")
-// res.set({'Content-Type' : 'application/json'})
-// console.log(res.header['Content-Type'])
-// console.log(req.headers[)
-// res.json(["warmonger"])
-// res.render('about',{name : 'flavio'})
-// })
-// app.listen(5000,()=> {
-//     console.log("server started on port 5000")
-// })
