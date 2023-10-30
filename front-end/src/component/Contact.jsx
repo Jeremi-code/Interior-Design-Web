a96d470bf71791e63e461ad74946539146457ddf
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 10:03:29 -0700

    use fetch api to get the data

--- front-end/src/component/Contact.jsx
+++ front-end/src/component/Contact.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
 import horizontal from "../css/MainBody.module.css";
 import classes from "../css/contact.module.css";
+
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const data = { name, email, message };
+    console.log(JSON.stringify(data)), console.log(data);
+    fetch("http://localhost:5000/sendMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className={classes.contact}>
       <h3 id="contact">Contacts</h3>
@@ -9,13 +31,25 @@ const Contact = () => {
         Do you want us to style your home? Fill out the form and fill me in with
         the details :) We love meeting new people!
       </p>
-      <form method="POST" action="/sendMessage">
-        <label for="name">Name</label>
-        <input type="text" />
-        <label for="email">Email</label>
-        <input type="text" />
-        <label for="Message">Message</label>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          onChange={(ev) => setName(ev.target.value)}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          onChange={(ev) => setEmail(ev.target.value)}
+          required
+        />
+        <label htmlFor="message">Message</label>
+        <input
+          type="text"
+          onChange={(ev) => setMessage(ev.target.value)}
+          required
+        />
         <input type="submit" value="Send Message" />
       </form>
     </div>
