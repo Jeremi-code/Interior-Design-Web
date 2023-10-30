46dcecf0972c7afb3def39135e12223822b621c9
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 13:16:00 -0700

    add the icon to the component but make it invisible till it's screen size decreases

--- front-end/src/component/NavigationMenu.jsx
+++ front-end/src/component/NavigationMenu.jsx
@@ -1,9 +1,14 @@
 import classes from "../css/NavBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 export default function NavigationMenu() {
   return (
     <div className={classes.NavBar}>
+      <div className={classes.compMenu}>
+      <FontAwesomeIcon icon = {faBars} className={classes.iconStyle}/>
       <div className={classes.CompanyName}>
         <p>Company Name</p>
+      </div>
       </div>
       <ul className={classes.NavMenu}>
         <li>
