d59d45fad690094c9053802d00dcf20bb741a706
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 10:19:48 -0700

    remove unwanted code

--- back-end/controller/MessageController.js
+++ back-end/controller/MessageController.js
@@ -2,12 +2,8 @@ import User from "../models/model.js";
 import asyncHandler from "express-async-handler";
 
 const sendMessage = asyncHandler(async (req, res) => {
-  console.log(typeof(req))
-  
-  const { name, email, message } = JSON.parse(req.body);
-  console.log(req.body);
+  const { name, email, message } = req.body;
   const user = await User.create({ name, email, message });
-  console.log(user);
   if (user) {
     res.status(201).json({
       _id: user._id,
