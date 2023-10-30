71ddc30b4053274b2b2d186ddcb8b6c241671214
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-10-30 10:01:55 -0700

    config dotenv to allow access into the .env file

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(conn)
-    } catch (error) {
-        console.log(`Error : ${error}`)
-        process.exit(1)
-    }
-}
-export default connectDB
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.log(`Error : ${error}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
