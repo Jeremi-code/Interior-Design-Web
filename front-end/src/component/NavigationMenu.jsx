f6cfab735a3e5b8bc2ea1fe08ef159bc980c0b99
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-02 12:58:04 -0700

    use useContext instead of useEffect and useState

--- front-end/src/component/NavigationMenu.jsx
+++ front-end/src/component/NavigationMenu.jsx
@@ -1,24 +1,24 @@
 import classes from "../css/NavBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState,useEffect,useContext } from "react";
+import { GlobalContext } from "../store/store";
 export default function NavigationMenu() {
-  const [isNavMenuOpen , setIsNavMenuOpen] = useState(false)
-  const handleOpenSidebar = () => {
-    setIsNavMenuOpen(!isNavMenuOpen)
-  }
-  const handleCloseSidebar = () => {
-    setIsNavMenuOpen(!isNavMenuOpen)
-  }
+
+  const {state} = useContext(GlobalContext)
+  const { handleNavBarClick } = useContext(GlobalContext)
+  console.log(state)
+  
   return (
-    <div className={classes.NavBar}>
+    <div className={classes.NavBar} >
       <div className={classes.compMenu}>
         <div style= {{display : 'flex',flexDirection : 'column'}}>
-        { !isNavMenuOpen && 
-      <FontAwesomeIcon icon = {faBars} className={classes.iconStyle} onClick = {() => handleOpenSidebar()}/>
-    }{ isNavMenuOpen && 
+        { !state.isNavBarClicked && 
+      <FontAwesomeIcon icon = {faBars} className={classes.iconStyle} onClick= {() => {handleNavBarClick()}}/>
+
+    }{state.isNavBarClicked && 
       <ul className={classes.toggleMenu} >
-        <p style={{fontSize:'22px',color:'white',marginBottom:'20px',textAlign : 'center',marginTop:'5px'}} onClick={() => handleCloseSidebar()}>Close Menu</p>
+        <p style={{fontSize:'22px',color:'white',marginBottom:'20px',textAlign : 'center',marginTop:'5px' }} className="p">Close Menu</p>
         <div className={classes.NavCompanyName}>
         <p>Company Name</p>
       </div>        
