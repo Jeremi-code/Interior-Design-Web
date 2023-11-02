d2a0180920c2bb60f2b55c6db535563dfe49f2f8
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-02 12:57:04 -0700

    create a store for managing the state of the opening and closing sidebar

---
+++ front-end/src/store/store.jsx
@@ -0,0 +1,39 @@
+import { createContext,useReducer } from "react"
+
+const initialState = {
+    isMainBodyClicked : false,
+    isNavBarClicked : false
+}
+export const GlobalContext = createContext (initialState)
+export const GlobalProvider = ({children}) => {
+    const [state,dispatch] = useReducer (Reducer,initialState)
+    const handleMainBodyClick = () => {
+            dispatch({type:'MAINBODY_CLICK'})
+    }
+    const handleNavBarClick = () => {
+        dispatch({type:'NAVBAR_CLICK'})
+    }
+    return (
+        <GlobalContext.Provider value={{state : state,handleNavBarClick,handleMainBodyClick}}>
+            {children}
+        </GlobalContext.Provider>
+    )
+} 
+
+const Reducer = (state,action) => {
+    switch(action.type) {
+        case 'MAINBODY_CLICK' :
+            if (state.isNavBarClicked) {
+                return {
+                    ...state,
+                    isNavBarClicked : state.isNavBarClicked
+                }
+            }
+            return state
+        case 'NAVBAR_CLICK' : 
+            return {
+                ...state,
+                isNavBarClicked :true
+            }
+    }
+}
