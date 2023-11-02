31e8c4213682ea67975620674653c9da66d20c9f
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-02 12:57:38 -0700

    wrap the whole components with a global context provider

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,14 @@
 import NavigationMenu from "./component/NavigationMenu";
 import MainBody from "./component/MainBody";
 import classes from "./App.module.css";
+import { GlobalProvider } from "./store/store";
 export default function App() {
   return (
     <div className={classes.whole}>
-      <NavigationMenu />
-      <MainBody />
+      <GlobalProvider>
+        <NavigationMenu />
+        <MainBody />
+      </GlobalProvider>
     </div>
   );
 }
