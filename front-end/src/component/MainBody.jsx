fd5a8188ee2c39fcfa0a2ac54f5ec8839d25e9f1
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-02 00:40:21 -0700

    change the misspelled word

--- front-end/src/component/MainBody.jsx
+++ front-end/src/component/MainBody.jsx
@@ -3,7 +3,7 @@ import ShowCase from "./ShowCase";
 import Services from "./Services";
 import Designer from "./Designer";
 import Packages from "./Packages";
-import Contact from "./contact";
+import Contact from "./Contact";
 export default function MainBody() {
   return (
     <div className={classes.main}>
