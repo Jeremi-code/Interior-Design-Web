f20469f2a32d23f578031650ecd8e6b5408ae42b
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-03 01:34:55 -0700

    import ToastContatiner context to use it to contact component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,12 +2,15 @@ import NavigationMenu from "./component/NavigationMenu";
 import MainBody from "./component/MainBody";
 import classes from "./App.module.css";
 import { GlobalProvider } from "./store/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App() {
   return (
     <div className={classes.whole}>
       <GlobalProvider>
         <NavigationMenu />
         <MainBody />
+        <ToastContainer />
       </GlobalProvider>
     </div>
   );
